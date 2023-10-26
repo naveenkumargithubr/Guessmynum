1c1e442ae5a86daedc651b99b0835b2720aa9619
naveen kumar <naveenkumarlanka77@gmail.com>
2023-10-26 10:58:17 +0530

    guess my number

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
 "use strict";
 
-// const msgEl = document.querySelector(".message");
-// msgEl.textContent = "Correct Number";
-// const msgElValue = msgEl.textContent; // to get the text value we use textContent
-// console.log(msgElValue);
-
-// const numberEl = document.querySelector(".number");
-// numberEl.textContent = 13;
-// const numvalue = numberEl.textContent;
-// console.log(numvalue);
-
-// const scoreEl = document.querySelector(".score");
-// scoreEl.textContent = 10;
-// const scorevalue = scoreEl.textContent;
-// console.log(scorevalue);
-
-// const inputEl = document.querySelector(".guess");
-// const inputVal = (inputEl.value = 23); // to get the input value we use value property
-// console.log(inputVal);
-
 // here we updating the random number
 let randomNumber = Math.floor(Math.random() * 20 + 1);
 let resscore = 20;
@@ -27,11 +8,17 @@ let highscore = 0;
 const checkEL = document.querySelector(".check");
 const againEl = document.querySelector(".again");
 
+// to display the res message here
+const displayMessage = function (message) {
+  const msgEl = document.querySelector(".message");
+  msgEl.textContent = message;
+};
+
+// clear button
 againEl.addEventListener("click", () => {
   resscore = 20;
   randomNumber = Math.floor(Math.random() * 20 + 1);
-  const msgEl = document.querySelector(".message");
-  msgEl.textContent = "Start guessing";
+  displayMessage("Start guessing");
   const scoreEl = document.querySelector(".score");
   scoreEl.textContent = resscore;
   const numberEl = document.querySelector(".number");
@@ -42,9 +29,9 @@ againEl.addEventListener("click", () => {
   bodyEl.style.backgroundColor = "#222";
 });
 
+// check buttton
 checkEL.addEventListener("click", () => {
   const inputEl = document.querySelector(".guess");
-  const msgEl = document.querySelector(".message");
   const scoreEl = document.querySelector(".score");
   const numberEl = document.querySelector(".number");
   const highscoreEl = document.querySelector(".highscore");
@@ -54,48 +41,50 @@ checkEL.addEventListener("click", () => {
 
   // when there is no input
   if (!inputVal) {
-    msgEl.textContent = "Please Enter Number";
-
+    displayMessage("Please Enter Number");
     // when player wins
   } else if (inputVal === randomNumber) {
-    msgEl.textContent = "Correct Number!";
+    displayMessage("Correct Number!");
     numberEl.textContent = randomNumber;
     bodyEl.style.backgroundColor = "#60b347";
     highscoreEl.textContent = resscore;
 
-    // updating the new hihh score
+    // updating the new high score
     if (resscore > highscore) {
       highscore = resscore;
       const highscoreEl = document.querySelector(".highscore");
-      highscoreEl.textContent = highscore;
+      highscoreEl.textContent = highscore; // it display the new highscore
     }
 
     // when guess is wrong
   } else if (inputVal !== randomNumber) {
     if (resscore > 1) {
-      msgEl.textContent = inputVal > randomNumber ? "Too High" : "Too Low";
+      displayMessage(inputVal > randomNumber ? "Too High" : "Too Low");
       resscore -= 1;
-      scoreEl.textContent = resscore;
+      scoreEl.textContent = resscore; // it display the decreasing course
     } else {
-      msgEl.textContent = "You Loss The game";
+      displayMessage("You Loss The game");
     }
   }
-  //guessing number too high
-  // else if (inputVal > randomNumber) {
-  //   // for loss the game
-  //   if (resscore > 1) {
-  //     msgEl.textContent = "Too High";
-  //     resscore -= 1;
-  //     scoreEl.textContent = resscore;
-  //   } else {
-  //     msgEl.textContent = "You Loss The game";
-  //   }
-
-  //   // guessing number is too low
-  // } else if (inputVal < randomNumber) {
-  //   // for loss the game
-  //   msgEl.textContent = "Too Low";
-  //   resscore -= 1;
-  //   scoreEl.textContent = resscore;
-  // }
 });
+
+// the below code is same as when guess is wrong code above
+
+//guessing number too high
+// else if (inputVal > randomNumber) {
+//   // for loss the game
+//   if (resscore > 1) {
+//     msgEl.textContent = "Too High";
+//     resscore -= 1;
+//     scoreEl.textContent = resscore;
+//   } else {
+//     msgEl.textContent = "You Loss The game";
+//   }
+
+//   // guessing number is too low
+// } else if (inputVal < randomNumber) {
+//   // for loss the game
+//   msgEl.textContent = "Too Low";
+//   resscore -= 1;
+//   scoreEl.textContent = resscore;
+// }
